cd5f8c0c226f107679eb7ddcc83968cc1e6550b3
Ulf Winkelvos <code@ulf.winkelvos.de>
2017-11-26 05:31:55 +0100

    add parameters

--- fs/init.js
+++ fs/init.js
@@ -5,32 +5,36 @@ load('api_timer.js');
 load("api_sys.js");
 load("api_net.js");
 load("api_file.js");
+load("api_esp8266.js");
 
-let pin = Cfg.get('app.pin');
-let dht = DHT.create(pin, DHT.DHT11);
+let APP_PIN = Cfg.get('app.pin');
+let APP_SAMPLE_INTERVAL = Cfg.get('app.sampleInterval');
+let APP_SAMPLE_COUNT = Cfg.get('app.sampleCount');
+let APP_SLEEP_INTERVAL = Cfg.get('app.sleepInterval');
+
+let dht = DHT.create(APP_PIN, DHT.DHT11);
 
 let ev = null;
+let sampleCounter = 0;
+let timer = null;
+
 Net.setStatusEventHandler(function (ev, arg) {
-	print("== Net event:", ev);
+	if (ev === Net.STATUS_GOT_IP) {
+		print("####### connected!");
+		timer = Timer.set(APP_SAMPLE_INTERVAL, true, function () {
+			sampleCounter++;
+			let data = {
+				date: Timer.now(),
+				temp: dht.getTemp(),
+				humidity: dht.getHumidity()
+			};
+			print("##### data: " + JSON.stringify(data));
+			File.write(JSON.stringify(data.date) + ";" + JSON.stringify(data.temp) + ";" + JSON.stringify(data.humidity) + "\n", "data.csv", "a");
+			if (sampleCounter >= APP_SAMPLE_COUNT) {
+				Timer.del(timer);
+				print("####### going to sleep!");
+				ESP8266.deepSleep(APP_SLEEP_INTERVAL);
+			}
+		}, null);
+	}
 }, null);
-
-RPC.addHandler('Sensors.Temp.Read', function (args) {
-	return dht.getTemp();
-});
-
-RPC.addHandler('Sensors.Humidity.Read', function (args) {
-	return dht.getHumidity();
-});
-
-
-//Sys.usleep(1000);
-let data = {
-	date: Timer.now(),
-	temp: dht.getTemp(),
-	humidity: dht.getHumidity()
-};
-print("##### data: " + JSON.stringify(data));
-File.write(JSON.stringify(data), "data.json", "a");
-let newdata = JSON.parse(File.read("data.json"));
-print("##### newdata: " + JSON.stringify(newdata));
-
